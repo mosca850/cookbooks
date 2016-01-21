4ec3c373de700a14e29d200837111b77108418e5
venturel <luca.venturelli@aeriagames.com>
2016-01-21 17:48:19 +0100

    removing metadata

--- metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'cookbooks'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures mesos-master'
-long_description 'Installs/Configures mesos-master'
-version          '0.1.0'
