3a44d44d3e83eef019a8111a8e7f9f7dc3894962
venturel <luca.venturelli@aeriagames.com>
2016-01-24 20:18:22 +0100

    adding templates

--- mesos/recipes/install.rb
+++ mesos/recipes/install.rb
@@ -84,7 +84,73 @@ template 'mesos-slave-init' do
   variables(name:    'mesos-slave',
             wrapper: '/etc/mesos-chef/mesos-slave')
 end
+case node['mesos']['init']
+when 'upstart'
+  template '/etc/default/zookeeper' do
+    source 'environment-defaults.erb'
+    owner 'zookeeper'
+    group 'zookeeper'
+    action :create
+    mode '0644'
+    notifies :restart, 'service[zookeeper]', :delayed
+  end
+  template '/etc/init/zookeeper.conf' do
+    source 'zookeeper.upstart.erb'
+    owner 'root'
+    group 'root'
+    action :create
+    mode '0644'
+    notifies :restart, 'service[zookeeper]', :delayed
+  end
+  service 'zookeeper' do
+    provider Chef::Provider::Service::Upstart
+    supports :status => true, :restart => true, :reload => true
+    action :enable
+  end
+when 'runit'
+  # runit_service does not install runit itself
+  include_recipe "runit"
 
+  runit_service 'zookeeper' do
+    default_logger true
+    options(
+      exec: executable_path
+    )
+    action [:enable, :start]
+  end
+when 'systemd'
+  template '/etc/default/zookeeper' do
+    source 'environment-defaults.erb'
+    owner 'zookeeper'
+    group 'zookeeper'
+    action :create
+    mode '0644'
+    notifies :restart, 'service[zookeeper]', :delayed
+  end
+  template '/etc/init.d/zookeeper' do
+    source 'zookeeper.sysv.erb'
+    owner 'root'
+    group 'root'
+    action :create
+    mode '0755'
+    notifies :restart, 'service[zookeeper]', :delayed
+  end
+
+  service_provider = value_for_platform_family(
+    'rhel'    => Chef::Provider::Service::Init::Redhat,
+    'default' => Chef::Provider::Service::Init::Debian
+  )
+
+  service 'zookeeper' do
+    provider service_provider
+    supports :status => true, :restart => true, :reload => true
+    action :enable
+  end
+when 'exhibitor'
+  Chef::Log.info('Assuming Exhibitor will start up Zookeeper.')
+else
+  Chef::Log.error('You specified an invalid service style for Zookeeper, but I am continuing.')
+end
 # Reload systemd on template change
 execute 'systemctl-daemon-reload' do
   command '/bin/systemctl --system daemon-reload'
