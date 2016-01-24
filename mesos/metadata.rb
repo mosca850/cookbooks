d32af484ed5d3908448e59334e5b3acd0b8a561c
venturel <luca.venturelli@aeriagames.com>
2016-01-24 19:00:06 +0100

    adding templates

--- mesos/metadata.rb
+++ mesos/metadata.rb
@@ -5,3 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures mesos'
 long_description 'Installs/Configures mesos'
 version          '0.1.0'
+%w( java apt ).each do |cb|
+  depends cb
+end
+depends 'yum', '~> 3.0'
